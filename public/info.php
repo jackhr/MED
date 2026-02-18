af575a571ae6a25f671ca7f5fcd622ac109d84f1
jackhr <jc2o@mac.com>
2026-02-18 09:19:18 -0500

    Add authentication for info.php

--- public/info.php
+++ public/info.php
@@ -2,4 +2,10 @@
 
 declare(strict_types=1);
 
+require_once dirname(__DIR__) . '/src/Env.php';
+require_once dirname(__DIR__) . '/src/Auth.php';
+
+Auth::startSession();
+Auth::requireAuthForPage('login.php');
+
 phpinfo();
